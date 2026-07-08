7879a60761de9e9bcc44c69143d034f18e3b33c2
adereskita <ade.reskita@bni.co.id>
2026-07-08 16:00:07 +0700

    remove string char to base

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://adereskita.github.io',
-  base: '/',
+  base: '',
   vite: {
     plugins: [tailwindcss()],
   },
